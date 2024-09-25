4afd48a76b9db659a7095b152061da87307b7032
mizorehe70dayo <biersnerhampsonii@gmail.com>
2024-09-25 20:51:47 +0800

    Pay支付微服务编码01

--- cloud-provider-payment8001/src/main/java/com/mahiru/cloud/entities/PayDTO.java
+++ cloud-provider-payment8001/src/main/java/com/mahiru/cloud/entities/PayDTO.java
@@ -19,12 +19,20 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class PayDTO implements Serializable {
     private Integer id;
-    //支付流水号
+    /**
+     * 支付流水号
+     */
     private String payNo;
-    //订单流水号
+    /**
+     * 订单流水号
+     */
     private String orderNo;
-    //用户账号ID
+    /**
+     * 用户账号ID
+     */
     private Integer userId;
-    //交易金额
+    /**
+     * 交易金额
+     */
     private BigDecimal amount;
 }
